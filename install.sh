3fb5fadf2dbca1ab7035db6207f70f49739dd21a
flus38765 <flus38765@gmail.com>
2018-06-10 13:39:39 +0700

    change setting

--- install.sh
+++ install.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
-read -e -p "Enter Website title (e.g. Iquidus) : " WEBSITE_TITLE
-read -e -p "Enter Website port (e.g. 3001) : " WEBSITE_PORT
-read -e -p "Enter Coin Name (e.g. Aither) : " COIN_NAME
-read -e -p "Enter Coin Symbol (e.g. AIT) : " COIN_SYMBOL
-read -e -p "Enter Mongo DB Name (e.g. explorerdb) : " MONGO_DB_NAME
-read -e -p "Enter Mongo DB User (e.g. iquidus) : " MONGO_DB_USER
-read -e -p "Enter Mongo DB Pass (e.g. 3xp!0reR) : " MONGO_DB_PASS
-read -e -p "Enter Wallet RPC Port (e.g. 40999) : " WALLET_PORT
-read -e -p "Enter Wallet RPC User (e.g. aithercoin) : " WALLET_USER
-read -e -p "Enter Wallet RPC Pass (e.g. 123gfjk3R3pCCVjHtbRde2s5kzdf233sa) : " WALLET_PASS
+read -e -p "Enter Website port (default. 3001) : " WEBSITE_PORT
+if [[ $WEBSITE_PORT == "" ]]; then
+	WEBSITE_PORT=3001
+fi
+read -e -p "Enter Coin Name (default. Aither) : " COIN_NAME
+if [[ $COIN_NAME == "" ]]; then
+	COIN_NAME="Aither"
+fi
+read -e -p "Enter Coin Symbol (default. AIT) : " COIN_SYMBOL
+if [[ $COIN_SYMBOL == "" ]]; then
+	COIN_SYMBOL="AIT"
+fi
+read -e -p "Enter Mongo DB Name (default. explorerdb) : " MONGO_DB_NAME
+if [[ $MONGO_DB_NAME == "" ]]; then
+	MONGO_DB_NAME="explorerdb"
+fi
+read -e -p "Enter Mongo DB User (default. aithercoin) : " MONGO_DB_USER
+if [[ $MONGO_DB_USER == "" ]]; then
+	MONGO_DB_USER="aithercoin"
+fi
+read -e -p "Enter Mongo DB Pass (default. 3xp!0reR) : " MONGO_DB_PASS
+if [[ $MONGO_DB_PASS == "" ]]; then
+	MONGO_DB_PASS="3xp!0reR"
+fi
+read -e -p "Enter Wallet RPC Port (default. 40999) : " WALLET_PORT
+if [[ $WALLET_PORT == "" ]]; then
+	WALLET_PORT="40999"
+fi
+read -e -p "Enter Wallet RPC User (default. aithercoin) : " WALLET_USER
+if [[ $WALLET_USER == "" ]]; then
+	WALLET_USER="aithercoin"
+fi
+read -e -p "Enter Wallet RPC Pass (default. 123gfjk3R3pCCVjHtbRde2s5kzdf233sa) : " WALLET_PASS
+if [[ $WALLET_PASS == "" ]]; then
+	WALLET_PASS="123gfjk3R3pCCVjHtbRde2s5kzdf233sa"
+fi
 echo ####################################################
 echo ####### Install nodejs #############################
 echo ####################################################
@@ -22,6 +48,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update -y
 sudo apt-get install -y mongodb-org
+service mongod start
 echo "use $MONGO_DB_NAME" > install-mongo.js
 echo 'db.createUser( { user: "'$MONGO_DB_USER'", pwd: "'$MONGO_DB_PASS'", roles: [ "readWrite" ] } )' >> install-mongo.js
 echo 'exit' >> install-mongo.js
@@ -33,7 +60,6 @@ echo ####################################################
 rm -f package-lock.json
 npm install --production
 cp settings.json.template settings.json
-sed -i -- 's/"title": "IQUIDUS"/"title": "'$WEBSITE_TITLE'"/g' settings.json
 sed -i -- 's/3001/'$WEBSITE_PORT'/g' settings.json
 sed -i -- 's/Darkcoin/'$COIN_NAME'/g' settings.json
 sed -i -- 's/DRK/'$COIN_SYMBOL'/g' settings.json
